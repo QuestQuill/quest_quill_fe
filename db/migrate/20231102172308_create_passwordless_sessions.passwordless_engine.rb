ccf6fdab840ede639772aea4ee112dd2a50bb493
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-02 14:22:24 -0600

    feat: add users table (will be changed)
    Co-authored-by: Dylan Timmons <129700694+DylanScotty@users.noreply.github.com>

---
+++ db/migrate/20231102172308_create_passwordless_sessions.passwordless_engine.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# This migration comes from passwordless_engine (originally 20171104221735)
+class CreatePasswordlessSessions < ActiveRecord::Migration[5.1]
+  def change
+    create_table(:passwordless_sessions) do |t|
+      t.belongs_to(
+        :authenticatable,
+        polymorphic: true,
+        index: {name: "authenticatable"}
+      )
+
+      t.datetime(:timeout_at, null: false)
+      t.datetime(:expires_at, null: false)
+      t.datetime(:claimed_at)
+      t.string(:token_digest, null: false)
+
+      t.timestamps
+    end
+  end
+end
