edb59b2073b1cef339d1b0d2a07b43defa98042e
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-06 13:45:42 -0700

    feat: facade for users

---
+++ app/facades/user_facade.rb
@@ -0,0 +1,31 @@
+class UserFacade
+  def initialize(user)
+    @user = user
+  end
+
+  def login_user(credentials)
+    json = service.login_authorization(credentials)
+
+    json[:user_id]
+  end
+
+  def user
+    json = service.get_user(@user)
+
+    User.new(json)
+  end
+
+  def create_user(params)
+    json = service.post_user(params)
+    
+    if !json.nil?
+      User.new(json)
+    else
+      nil
+    end
+  end
+
+  def service
+    DndService.new
+  end
+end
