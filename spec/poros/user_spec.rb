0a93fd994415938e47d95b28d0f7564e50017904
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-08 15:33:36 -0700

    feat/test: update poro for oauth login and update tests

--- spec/poros/user_spec.rb
+++ spec/poros/user_spec.rb
@@ -3,20 +3,30 @@ require "rails_helper"
 RSpec.describe User do
   describe "initialize" do
     it "creates a user object" do
-      VCR.use_cassette("DO_NOT_DELETE_create_user_waterbottle_poro") do
-        user_data = {
-          username: "waterbottle",
-          email: "waterbottle@gmail.com",
-          password: "water"
+      user_data = {
+        id: "5",
+        attributes: {
+          uid: "987923",
+          username: "plate",
+          email: "plate@gmail.com",
+          token: "2kdhfal",
+          token_expiration: "DHFKASDHF",
+          auth_token: "hdks2974",
+          campaigns: ["campaign 1", "campaign 2"]
         }
+      }
 
-        user = UserFacade.new(nil).create_user(user_data)
+      user = User.new(user_data)
 
-        expect(user).to be_a(User)
-        expect(user.id).to eq(user.id)
-        expect(user.username).to eq("waterbottle")
-        expect(user.email).to eq("waterbottle@gmail.com")
-      end
+      expect(user).to be_a(User)
+      expect(user.id).to eq(user_data[:id])
+      expect(user.uid).to eq(user_data[:attributes][:uid])
+      expect(user.username).to eq(user_data[:attributes][:username])
+      expect(user.email).to eq(user_data[:attributes][:email])
+      expect(user.token).to eq(user_data[:attributes][:token])
+      expect(user.token_expiration).to eq(user_data[:attributes][:token_expiration])
+      expect(user.auth_token).to eq(user_data[:attributes][:auth_token])
+      expect(user.campaigns).to eq(user_data[:attributes][:campaigns])
     end
   end
 end
